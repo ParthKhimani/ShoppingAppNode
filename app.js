87156776d4288599846c20c8956afd73cb816e4a
parth.khimani <parthkhimani48@gmail.com>
2023-09-28 16:52:37 +0530

    few changes

--- app.js
+++ app.js
@@ -1,22 +1,14 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-
+const cors = require("cors");
 const router = require("./Routes/router");
 
 const app = express();
 const MONGO_URI =
   "mongodb+srv://parth:P%40rth2005@cluster0.eixcpta.mongodb.net/WebElight?retryWrites=true&w=majority";
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Method",
-    "GET,PUT,POST,PATCH,DELETE,OPTIONS"
-  );
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  next();
-});
+app.use(cors());
 app.use(bodyParser.json());
 app.use(router);
 
